e167260290ce4d6ed68a0a0f117934fdb45e102b
KhomerikiB <46321792+KhomerikiB@users.noreply.github.com>
2019-01-27 20:30:29 +0400

    Updates

---
+++ precache-manifest.41e1d8f4767fefe1e88387e8107cf606.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "da7bec0254bbc76338d6b3b3abac3f44",
+    "url": "/portfolio/static/media/medium.da7bec02.woff"
+  },
+  {
+    "revision": "acca77cc06a291e593c9f53c19deb405",
+    "url": "/portfolio/static/media/light.acca77cc.woff"
+  },
+  {
+    "revision": "ad738b8fce7c6da1ebbc46571a523842",
+    "url": "/portfolio/static/media/heavy.ad738b8f.woff"
+  },
+  {
+    "revision": "1ef7cbe8736da6fde4c6f1abcb2ed8fa",
+    "url": "/portfolio/static/media/c.1ef7cbe8.jpg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "031be99e592e5d2ae2ff",
+    "url": "/portfolio/static/js/main.031be99e.chunk.js"
+  },
+  {
+    "revision": "5aee3ba8a84262d33f45",
+    "url": "/portfolio/static/js/1.5aee3ba8.chunk.js"
+  },
+  {
+    "revision": "031be99e592e5d2ae2ff",
+    "url": "/portfolio/static/css/main.48685b4b.chunk.css"
+  },
+  {
+    "revision": "aafec8febb2ad9758d712d37031bb183",
+    "url": "/portfolio/index.html"
+  }
+];
