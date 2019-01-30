97580cedf10d6459f52314c6f21ace7f664c532a
KhomerikiB <46321792+KhomerikiB@users.noreply.github.com>
2019-01-30 23:20:02 +0400

    innitial commit

--- src/components/fixedelements/overlay/Overlay.js
+++ src/components/fixedelements/overlay/Overlay.js
@@ -3,11 +3,16 @@ import "./Overlay.css";
 
 const Overlay = props => {
   let className = "";
-  if (props.currentPage === "/") {
+  if (
+    props.currentPage === "/portfolio/" ||
+    props.currentPage === "/" ||
+    props.currentPage === "/portfolio"
+  ) {
     className = "Overlay overlay-extra";
   } else {
     className = "Overlay";
   }
+  console.log(props.currentPage);
   return <div className={className} />;
 };
 export default Overlay;
