f74735b6319df5a8814f44715af0cdebc0aedd83
KhomerikiB <46321792+KhomerikiB@users.noreply.github.com>
2019-03-02 14:40:02 +0400

    innitial commit

--- src/components/work/project/Project.js
+++ src/components/work/project/Project.js
@@ -12,7 +12,7 @@ class Project extends React.Component {
           title="Hotelweb"
           languages="#JavaScript #HTML5 #CSS3"
           currentPage={this.props.currentPage}
-          href={"hhtps://hotelweb.ge/"}
+          href={"https://hotelweb.ge/"}
         />
 
         <ProjectItem
